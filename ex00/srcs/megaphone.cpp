db85a8c6e7d71c4d57b640e78d6aca2050c259f6
Margaux Riant <mriant@student.42.fr>
2022-08-15 15:49:13 +0200

    Add a second way of doing in the ex00.
    Reorganize repo structure
     On branch master
     Changes to be committed:
    	new file:   .gitignore
    	renamed:    Makefile -> ex00/Makefile
    	renamed:    srcs/megaphone.cpp -> ex00/srcs/megaphone.cpp

--- ex00/srcs/megaphone.cpp
+++ ex00/srcs/megaphone.cpp
@@ -6,35 +6,55 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 10:35:32 by mriant            #+#    #+#             */
-/*   Updated: 2022/08/15 14:07:16 by mriant           ###   ########.fr       */
+/*   Updated: 2022/08/15 15:44:09 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
+// int	main(int ac, char **av)
+// {
+// 	std::string	result = "";
+// 	int			i;
+// 	int			j;
+
+// 	if (ac == 1)
+// 	{
+// 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+// 		return (0);
+// 	}
+// 	i = 1;
+// 	while (av[i])
+// 	{
+// 		j = 0;
+// 		while (av[i][j])
+// 		{
+// 			result.push_back(std::toupper(av[i][j]));
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// 	std::cout << result << std::endl;
+// 	return (0);
+// }
+
 int	main(int ac, char **av)
 {
-	std::string	result = "";
-	int			i;
-	int			j;
-
 	if (ac == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	i = 1;
-	while (av[i])
+	for (int i = 1; i < ac; i++)
 	{
-		j = 0;
-		while (av[i][j])
+		std::string	str = av[i];
+		for (std::string::iterator it = str.begin(); it != str.end(); it++)
 		{
-			result.push_back(std::toupper(av[i][j]));
-			j++;
+			std::string s(1, std::toupper(*it));
+			std::cout << s;
 		}
-		i++;
 	}
-	std::cout << result << std::endl;
+	std::cout << std::endl;
 	return (0);
 }
