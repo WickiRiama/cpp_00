b1c9f89e497207b9a6dcda59aa5b5af3abcce319
Margaux Riant <mriant@student.42.fr>
2022-08-29 17:28:11 +0200

    WIP in 'search' function
     On branch master
     Changes to be committed:
    	modified:   ex01/Makefile
    	new file:   ex01/includes/phonebook.h
    	modified:   ex01/srcs/PhoneBook.cpp
    	modified:   ex01/srcs/main.cpp

--- ex01/includes/phonebook.h
+++ ex01/includes/phonebook.h
@@ -1,37 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   phonebook.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/15 16:26:21 by mriant            #+#    #+#             */
-/*   Updated: 2022/08/29 17:25:12 by mriant           ###   ########.fr       */
+/*   Created: 2022/08/29 16:14:42 by mriant            #+#    #+#             */
+/*   Updated: 2022/08/29 16:18:10 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include "phonebook.h"
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
 
-void	print_short(std::string str)
-{
-	if (str.size() > 9)
-	{
-		str.resize(9);
-		str.push_back('.');
-	}
-	std::cout << std::setw(10) << str;
-	return ;
-}
+# include "PhoneBook.hpp"
 
-int	main(void)
-{
-	PhoneBook	pb1;
+void	print_short(std::string str);
 
-	// pb1.add();
-	// pb1.add();
-	pb1.search();
-	return 0;
-}
+#endif
--- ex01/srcs/PhoneBook.cpp
+++ ex01/srcs/PhoneBook.cpp
@@ -6,13 +6,16 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 16:53:37 by mriant            #+#    #+#             */
-/*   Updated: 2022/08/15 18:07:16 by mriant           ###   ########.fr       */
+/*   Updated: 2022/08/29 17:27:14 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
 
-#include "PhoneBook.hpp"
+#include "phonebook.h"
 
 PhoneBook::PhoneBook(void)
 {
@@ -23,32 +26,70 @@ PhoneBook::PhoneBook(void)
 
 PhoneBook::~PhoneBook(void)
 {
+	std::cout << "PhoneBook destroyed" << std::endl;
 	return ;
 }
 
 void	PhoneBook::add()
 {
-	std::string	first_name;
-	std::string	last_name;
-	std::string	nickname;
+	std::string	first_name("");
+	std::string	last_name("");
+	std::string	nickname("");
 
-	std::cout << "Contact first name : ";
-	std::cin >> first_name;
-	std::cout << "Contact last name : ";
-	std::cin >> last_name;
-	std::cout << "Contact  nickname : ";
-	std::cin >> nickname;
-
+	while (first_name.size() == 0)
+	{
+		std::cout << "Contact first name : ";
+		std::getline(std::cin, first_name);
+	}
+	while (last_name.size() == 0)
+	{
+		std::cout << "Contact last name : ";
+		std::getline(std::cin, last_name);
+	}
+	while (nickname.size() == 0)
+	{
+		std::cout << "Contact  nickname : ";
+		std::getline(std::cin, nickname);
+	}
 	Contact	new_contact(this->nb_contacts, first_name, last_name, nickname);
 	this->contact_tab[this->nb_contacts % 8] = new_contact;
 	this->nb_contacts++;
-
-	std::cout << "New Contact: " << this->contact_tab[0].index << this->contact_tab[0].first_name << this->contact_tab[0].last_name << std::endl;
 	return ;
 }
 
 void	PhoneBook::search()
 {
+	std::string	short_str;
+	std::string	index_str = "";
+	int			index;
+	int			i;
+
+	i = 0;
+	if (this->nb_contacts == 0)
+	{
+		std::cout << "There is no registered contact" << std::endl;
+		return ;
+	}
+	while (i < 8 && this->contact_tab[i].first_name != "")
+	{
+		std::cout << std::setw(10) << i;
+		std::cout << '|';
+		print_short(this->contact_tab[i].first_name);
+		std::cout << '|';
+		print_short(this->contact_tab[i].last_name);
+		std::cout << '|';
+		print_short(this->contact_tab[i].nickname);
+		std::cout << std::endl;
+		i++;
+	}
+	while (index_str.size() == 0 || index > i - 1 || index < 0)
+	{
+		std::cout << "Index of the contact to display: ";
+		std::getline(std::cin, index_str);
+		index = atoi(index_str.c_str());
+		if (index > i - 1 || index < 0)
+			std::cout << "Thhis index does not exist" << std::endl;
+	}
 	return ;
 }
 
